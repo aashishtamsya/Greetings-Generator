1f600a5f1f04b29e2df257d553962aef80a054fa
Aashish Tamsya <aashish.tamsya@gmail.com>
2017-07-19 18:06:44 +0530

    Initial Commit

---
+++ Greetings Generator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Greetings Generator
+//
+//  Created by Aashish Tamsya on 19/07/17.
+//  Copyright © 2017 Aashish Tamsya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
